501dc8cbc009be61ce1fcdd11750947589831e2c
Janusz Bialy <jbialy@gmail.com>
2017-10-17 11:33:00 -0400

    include a few comments

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+# import resource modules
 from api.resources.hello import Hello
 
 # init a Flask app
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myawesome.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./myawesome.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # init flask_restful
@@ -18,7 +19,8 @@ db = SQLAlchemy(app)
 # import database models
 from api.models.post import Post
 
-# create the db tables
+# create the db tables (this will not overwrite an existing database) 
+#
 db.create_all()
 
 # expose a basic resource
