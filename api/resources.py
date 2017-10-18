fdad4b4dba5ce7e4791a0d3db3322fc500130633
Janusz Bialy <jbialy@gmail.com>
2017-10-18 10:33:14 -0400

    added palindrome check

--- api/resources.py
+++ api/resources.py
@@ -29,7 +29,20 @@ class PostResource(Resource):
     def get(self, post_id):
         # change to to better utilize the PostModel object?
         result = PostModel.query.filter_by(id=post_id).first()
-        return { 'user_id' : result.user_id, 'post' : result.message}
+        ## let's check if the post is a palindrome 
+        message = result.message
+        # remove spaces
+        message_stripped = message.replace(" ", "")
+        # travers the string, comparing char at pos[0+N] to pos[STRLENGTH-N]
+        # if all chars are equal then we have a palindrome!
+        is_palindrome = True
+        for pos, char in enumerate(message_stripped):
+            msg_len = len(message_stripped)-1
+            if (char != message_stripped[msg_len-pos]):
+                # as soon as we don't find a match break out
+                is_palindrome = False
+                break
+        return { 'user_id' : result.user_id, 'post' : result.message, "palindrome" : is_palindrome}
 
     def post(self):
         args = parser.parse_args()
