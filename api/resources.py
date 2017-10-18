9c3bf34558f2b49483c4875c761457636db814b1
Janusz Bialy <jbialy@gmail.com>
2017-10-17 20:50:19 -0400

    - added logic for posting a message - added logic for getting a post by id

--- api/resources.py
+++ api/resources.py
@@ -1,8 +1,18 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from api.models import PostModel
 
+# create a request parser and add argument that it should look for in the request
+parser = reqparse.RequestParser()
+# we need the request to contain a user_id and a message
+parser.add_argument('user_id', type=int, required=True)
+parser.add_argument('message', required=True)
+
+#
+# define /posts endpoint
+#  
 class PostListResource(Resource):
     def get(self):
+        # fetch all the posts
         posts = PostModel.query.all()
         results = []
         # go through all the post objects and add them as individual lists to an array
@@ -10,9 +20,19 @@ class PostListResource(Resource):
             obj = {'id': post.id, 'user_id': post.user_id, 'message': post.message}
             results.append(obj)
         return results
-
+#
+# define /post endpoint
+#  
 class PostResource(Resource):
-    def get(self):
-        newPost = PostModel(12, "this is my post number one")
-        newPost.save()
-        return {'heloooo': 'wwwworld'}
+    def get(self, post_id):
+        # change to to better utilize the PostModel object?
+        result = PostModel.query.filter_by(id=post_id).first()
+        return { 'user_id' : result.user_id, 'post' : result.message}
+
+    def post(self):
+        args = parser.parse_args()
+        # create a new post using the arguments received and save it!
+        new_post = PostModel(args['user_id'], args['message'])
+        new_post.save()
+        # simply return the new post
+        return {int(args['user_id']) : args['message']}, 201
